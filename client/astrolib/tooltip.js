b9d932dde3674e271df96915359c0281ae585457
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2023-03-17 10:07:45 +0000

    Amended logging to save memory

--- client/astrolib/tooltip.js
+++ client/astrolib/tooltip.js
@@ -1,9 +1,19 @@
 const tooltip = document.querySelector('#tooltip');
 
+let isPackaged = false;
+window.API.isPackaged().then(packaged => {
+    isPackaged = packaged;
+    console.log(`Logging on the renderer will be: ${packaged ? 'disabled' : 'enabled'}!`);
+});
+
+function log(msg) {
+    if (!isPackaged) console.log(msg);
+}
+
 function applyTooltips() {
     document.querySelectorAll('[data-tooltip]').forEach((e) => {
         if (e.hasAttribute('has-tooltip')) {
-            console.log('[astrolib/tooltip.js] Node already has tooltip applied, skipping...');
+            log('[astrolib/tooltip.js] Node already has tooltip applied, skipping...');
         } else {
             e.addEventListener('mouseenter', (e) => {
                 tooltip.style.display = 'block';
