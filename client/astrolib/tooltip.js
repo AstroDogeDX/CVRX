b976ecce524b2affb8a1c541579e1445d290a03d
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2023-03-30 19:17:07 +0100

    Minor refactor of Tooltip module

--- client/astrolib/tooltip.js
+++ client/astrolib/tooltip.js
@@ -2,43 +2,46 @@ const tooltip = document.querySelector('#tooltip');
 const overlay = document.querySelector('#tooltip-overlay');
 
 let isPackaged = false;
-window.API.isPackaged().then(packaged => {
-    isPackaged = packaged;
-    console.log(`Logging on the renderer will be: ${packaged ? 'disabled' : 'enabled'}!`);
-});
 
-function log(msg) {
+const log = (msg) => {
     if (!isPackaged) console.log(msg);
-}
+};
+
+const moveTooltip = (e) => {
+    const offsetX = e.clientX >= (window.innerWidth / 2) ? -Math.abs(tooltip.offsetWidth + 5) : 15;
+    const offsetY = e.clientY >= (window.innerHeight / 2) ? -Math.abs(tooltip.offsetHeight) : 15;
+    tooltip.style.left = `${e.clientX + offsetX}px`;
+    tooltip.style.top = `${e.clientY + offsetY}px`;
+};
+
+const showTooltip = (e) => {
+    tooltip.style.display = 'block';
+    tooltip.innerHTML = e.target.dataset.tooltip;
+};
+
+const hideTooltip = () => {
+    tooltip.style.display = 'none';
+    tooltip.innerHTML = '';
+};
 
 function applyTooltips() {
-    if (!overlay.hasAttribute('tooltips-enabled')) {
-        window.addEventListener('mousemove', (e) => {
-            let offsetX = e.clientX >= (window.innerWidth / 2) ? -Math.abs(tooltip.offsetWidth + 5) : 15;
-            let offsetY = e.clientY >= (window.innerHeight / 2) ? -Math.abs(tooltip.offsetHeight) : 15;
-            tooltip.style.left = `${e.clientX + offsetX}px`;
-            tooltip.style.top = `${e.clientY + offsetY}px`;
-        });
-        overlay.setAttribute('tooltips-enabled', 'true');
+    const TOOLTIPS_ENABLED = 'tooltips-enabled';
+    const HAS_TOOLTIP = 'has-tooltip';
+
+    if (!overlay.hasAttribute(TOOLTIPS_ENABLED)) {
+        window.addEventListener('mousemove', moveTooltip);
+        overlay.setAttribute(TOOLTIPS_ENABLED, 'true');
         log('[astrolib/tooltip.js] Tooltips Enabled');
     }
+
     document.querySelectorAll('[data-tooltip]').forEach((e) => {
-        if (e.hasAttribute('has-tooltip')) {
+        if (e.hasAttribute(HAS_TOOLTIP)) {
             log('[astrolib/tooltip.js] Node already has tooltip applied, skipping...');
         } else {
-            e.addEventListener('mouseenter', (e) => {
-                tooltip.style.display = 'block';
-                tooltip.innerHTML = e.target.dataset.tooltip;
-            });
-            e.addEventListener('mouseleave', () => {
-                tooltip.style.display = 'none';
-                tooltip.innerHTML = '';
-            });
-            e.addEventListener('mouseup', () => {
-                tooltip.style.display = 'none';
-                tooltip.innerHTML = '';
-            });
-            e.setAttribute('has-tooltip', 'true');
+            e.addEventListener('mouseenter', showTooltip);
+            e.addEventListener('mouseleave', hideTooltip);
+            e.addEventListener('mouseup', hideTooltip);
+            e.setAttribute(HAS_TOOLTIP, 'true');
         }
     });
 }
