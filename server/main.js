de5d5547723b7413805fe7316ca423279f009c81
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2023-09-14 02:46:24 +0100

    Added SysTray Tooltip & Notification

--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
-const { app, BrowserWindow, Menu, ipcMain, Tray } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain, Tray, Notification } = require('electron');
 
 // Prevent app launching multiple times during the installation
-if (require("electron-squirrel-startup")) {
+if (require('electron-squirrel-startup')) {
 	app.quit();
 	return;
 }
@@ -15,21 +15,21 @@ if (!app.requestSingleInstanceLock()) {
 	return;
 }
 
-const log = require("./logger").GetLogger("Main");
+const log = require('./logger').GetLogger('Main');
 
-const path = require("path");
+const path = require('path');
 
-const Config = require("./config");
-const { Core } = require("./data");
-const CVRWebsocket = require("./api_cvr_ws");
-const Cache = require("./cache");
-const Updater = require("./updater");
+const Config = require('./config');
+const { Core } = require('./data');
+const CVRWebsocket = require('./api_cvr_ws');
+const Cache = require('./cache');
+const Updater = require('./updater');
 
 // Remove the menu when the app is packaged
 if (app.isPackaged) Menu.setApplicationMenu(null);
 
 // Set the max limit for renderer process to 4092Mb
-app.commandLine.appendSwitch("js-flags", "--max-old-space-size=4092");
+app.commandLine.appendSwitch('js-flags', '--max-old-space-size=4092');
 
 // Temporary setting for close to taskbar feature
 const closeToTaskbar = true;
@@ -43,9 +43,9 @@ const CreateWindow = async () => {
 		minHeight: 600,
 		width: 1280,
 		height: 720,
-		icon: "./client/img/cvrx-ico.ico",
+		icon: './client/img/cvrx-ico.ico',
 		webPreferences: {
-			preload: path.join(__dirname, "preload.js"),
+			preload: path.join(__dirname, 'preload.js'),
 			devTools: !app.isPackaged,
 		},
 	});
@@ -57,7 +57,7 @@ const CreateWindow = async () => {
 	const core = new Core(mainWindow, app);
 
 	// and load the index.html of the app.
-	await mainWindow.loadFile("client/index.html");
+	await mainWindow.loadFile('client/index.html');
 
 	// Initialize Cache
 	Cache.Initialize(mainWindow);
@@ -69,7 +69,7 @@ const CreateWindow = async () => {
 			activeCredentials.Username,
 			activeCredentials.AccessKey,
 			true,
-			true
+			true,
 		);
 	} else {
 		await core.SendToLoginPage();
@@ -80,54 +80,59 @@ const CreateWindow = async () => {
 
 // Function returns the built tray object.
 const BuildTray = async (mainWindow) => {
-	log.info(`Building tray`);
+	log.info('Building tray');
 
 	// Set the tray icon
-	let tray = new Tray("./client/img/cvrx-ico.ico");
+	let tray = new Tray('./client/img/cvrx-ico.ico');
+
+	// Add tooltip to tray icon for clarity
+	tray.setToolTip('CVRX');
 
 	// Setup the tray menu functionality
 	tray.setContextMenu(
 		Menu.buildFromTemplate([
 			{
-				label: "Show CVRX",
+				label: 'Show CVRX',
 				click: function () {
 					mainWindow.show();
 				},
 			},
 			{
-				label: "Exit CVRX",
+				label: 'Exit CVRX',
 				click: function () {
-					isQuitting = true;
 					// We use destroy here to skip emitting the close event
 					// Source: https://www.electronjs.org/docs/latest/api/browser-window#windestroy
 					mainWindow.destroy();
 				},
 			},
-		])
+		]),
 	);
+	tray.on('double-click', () => {
+		mainWindow.show();
+	});
 };
 
 app.whenReady().then(async () => {
 	const mainWindow = await CreateWindow();
 	// Build tray menu
-	let tray = await BuildTray(mainWindow, app);
+	await BuildTray(mainWindow, app);
 
-	app.on("second-instance", () => {
+	app.on('second-instance', () => {
 		// Someone tried to run a second instance, we should focus our window.
 		if (mainWindow) {
 			if (mainWindow.isMinimized()) mainWindow.restore();
 			mainWindow.focus();
 		}
 	});
-	app.on("activate", () => {
+	app.on('activate', () => {
 		// On macOS, it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
 		if (BrowserWindow.getAllWindows().length === 0) {
 			CreateWindow();
 		}
 	});
-	mainWindow.webContents.on("will-prevent-unload", (event) => {
-		log.warn("will-prevent-unload", event);
+	mainWindow.webContents.on('will-prevent-unload', (event) => {
+		log.warn('will-prevent-unload', event);
 		// Todo: Does this prevent unloading?
 		// const choice = dialog.showMessageBoxSync(mainWindow, {
 		//     type: 'question',
@@ -142,14 +147,14 @@ app.whenReady().then(async () => {
 		//     event.preventDefault();
 		// }
 	});
-	mainWindow.webContents.on("render-process-gone", (event, detailed) => {
+	mainWindow.webContents.on('render-process-gone', (event, detailed) => {
 		log.warn(
-			"render-process-gone " +
+			'render-process-gone ' +
 				detailed.reason +
-				", exitCode = " +
-				detailed.exitCode
+				', exitCode = ' +
+				detailed.exitCode,
 		);
-		log.warn("render-process-gone", { event, detailed });
+		log.warn('render-process-gone', { event, detailed });
 		//  logger.info("!crashed, reason: " + detailed.reason + ", exitCode = " + detailed.exitCode)
 		// if (detailed.reason == "crashed"){
 		//     // relaunch app
@@ -159,13 +164,19 @@ app.whenReady().then(async () => {
 	});
 
 	// Override default close event to close to taskbar instead
-	mainWindow.on("close", (event, detailed) => {
+	mainWindow.on('close', (event) => {
 		// Prevent default action
 		event.preventDefault();
+
+		let minimizeNotification = {
+			title: 'CVRX',
+			body: 'CVRX is still running in the System Tray',
+		};
 
 		// Check if we should close to taskbar
 		if (closeToTaskbar) {
 			mainWindow.hide();
+			new Notification(minimizeNotification).show();
 		} else {
 			mainWindow.destroy();
 		}
@@ -174,21 +185,21 @@ app.whenReady().then(async () => {
 	await Updater.Setup(mainWindow);
 });
 
-app.on("ready", () => {
+app.on('ready', () => {
 	let appVersion = app.getVersion();
 	// Send version info to renderer process
-	ipcMain.on("get-app-version", (event) => {
-		event.sender.send("app-version", appVersion);
+	ipcMain.on('get-app-version', (event) => {
+		event.sender.send('app-version', appVersion);
 	});
 });
 
-app.on("window-all-closed", () => app.quit());
+app.on('window-all-closed', () => app.quit());
 
-app.on("will-quit", async () => {
+app.on('will-quit', async () => {
 	// This won't be called if the application is quit by a Windows shutdown/logout/restart
 	// On quitting let's close our socket if exist
 	await CVRWebsocket.DisconnectWebsocket(
 		true,
-		"The CVRX Application is closing..."
+		'The CVRX Application is closing...',
 	);
 });
